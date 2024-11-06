02fce5c9ecaf44059ba30b38b385500155fd85a6
Sylix <russiancatwithgun@gmail.com>
2024-11-06 09:42:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,136 +4,169 @@ import threading
 import colorama
 from colorama import *
 import os
-import requests
+import os
+import ctypes
 import ast
+import requests
 
-# Initialize colorama
+errir = print(f"{Fore.WHITE} [{Fore.RED}ERROR{Fore.WHITE}]{Fore.RESET} ")
+sucess = print(f"{Fore.WHITE} [{Fore.GREEN}SUCCESS{Fore.WHITE}]{Fore.RESET} ")
+consol = print(f"{Fore.WHITE} [{Fore.BLUE}CONSOLE{Fore.WHITE}]{Fore.RESET} ")
+
 init(convert=True)
+print(f"{Fore.WHITE}[{Fore.BLUE}CONSOLE{Fore.WHITE}]{Fore.RESET} LOADING MODULES AND ASSETS")
 
-# Global Variables
-spamming = False
-accpersec = 0
-threadsready = False
-workingproxies = 0
-
-# Colors
-errir = lambda: print(f"{Fore.WHITE} [{Fore.RED}ERROR{Fore.WHITE}] {Fore.RESET}")
-sucess = lambda: print(f"{Fore.WHITE} [{Fore.GREEN}SUCCESS{Fore.WHITE}] {Fore.RESET}")
-consol = lambda: print(f"{Fore.WHITE} [{Fore.BLUE}CONSOLE{Fore.WHITE}] {Fore.RESET}")
-
-# Loading Message
-consol()
-print(f"{Fore.WHITE}[{Fore.BLUE}CONSOLE{Fore.WHITE}] LOADING MODULES AND ASSETS")
-
-# Fetch Proxies
+print ("Getting Proxies")
 url = "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
 proxget = requests.get(url=url)
 proxlist = proxget.text.splitlines()
-print(f"{Fore.WHITE}[{Fore.BLUE}CONSOLE{Fore.WHITE}] FOUND {len(proxlist)} PROXIES")
+print ("Found Proxies")
 
-# Cache Proxies Functions
+spamming = False
+accpersec = 0
+
+threadsready = False
+
+workingproxies = 0
+
 def cache_proxies(proxies):
-    with open("cache_proxies.txt", "w") as w:
-        w.write(str(proxies))
+  with open("cache_proxies.txt", "w") as w:
+    w.write(str(proxies))
+    w.close()
 
 def read_cached_proxies():
-    with open("cache_proxies.txt", "r") as readFile:
-        proxies = readFile.read()
-    return ast.literal_eval(proxies)
+  with open("cache_proxies.txt", "r") as readFile:
+    proxies = readFile.read()
+    readFile.close()
+    return(proxies)
+  
+cache_proxlist = ast.literal_eval(read_cached_proxies())
 
-cache_proxlist = read_cached_proxies() if os.path.exists("cache_proxies.txt") else proxlist
-
-# Proxy Getter
 def get_http_proxy(index):
-    return proxlist[index]
+    retproxy = proxlist[index]
+    return(retproxy)
 
 def get_cached_proxy(index):
-    return cache_proxlist[index]
+  retproxy = cache_proxlist[index]
+  return(retproxy)
 
-# Spam PlayFab
 def spamPlayfab(titleID, prefix, spamProxy, useCache):
-    global workingproxies, accpersec, threadsready
-    while True:
-        if threadsready:
-            playfabAPIRoute = f"https://{titleID}.playfabapi.com/Client/LoginWithCustomID"
-            cid = f"{prefix}{random.randint(0, 999999)}"
-            payload = {
-                "TitleId": titleID,
-                "CreateAccount": True,
-                "CustomId": cid
-            }
+  global workingproxies
+  global accpersec
+  global threadsready
+  while 1 == 1:
+    if threadsready:
+      playfabAPIRoute = "https://{title_id}.playfabapi.com/Client/LoginWithCustomID".format(
+        title_id=titleID)
+      cid = "{prefix}{random}".format(prefix=prefix,random=str(random.randint(0, 999999)))
+      payload = {
+        "TitleId": titleID,
+        "CreateAccount": True,
+        "CustomId": cid
+      }
 
-            proxies = {
-                "http": spamProxy,
-                "https": spamProxy
-            }
+      proxies = {
+        "http" : spamProxy,
+        "https" : spamProxy
+      }
 
-            try:
-                response = requests.post(
-                    url=playfabAPIRoute,
-                    json=payload,
-                    proxies=proxies
-                )
-            except Exception as e:
-                print(f"Error with proxy {spamProxy}: {str(e)}")
-                if useCache:
-                    cache_proxlist.remove(spamProxy)
-                    cache_proxies(cache_proxlist)
-                else:
-                    proxlist.remove(spamProxy)
-                    cache_proxies(proxlist)
-                workingproxies -= 1
-                break
-
-            responseJson = response.json()
-
-            if responseJson.get('code') == 200:
-                print(f"{Fore.WHITE}[{Fore.LIGHTGREEN_EX}SUCCESS{Fore.WHITE}]{Fore.RESET} ACCOUNT CREATED: {responseJson['data']['EntityToken']['Entity']['Id']} WITH CUSTOM ID: {cid}")
-                accpersec += 1
-            elif responseJson.get('code') == 429:
-                print(f"{Fore.WHITE}[{Fore.RED}ERROR{Fore.WHITE}]{Fore.RESET} Rate limited or PlayFab storage full")
-            elif 'error' in responseJson:
-                if responseJson['error'] == 'EvaluationModePlayerCountExceeded':
-                    print(f"{Fore.WHITE}[{Fore.RED}ERROR{Fore.WHITE}]{Fore.RESET} PlayFab is full.")
-                else:
-                    print(f"Error: {responseJson}")
-
-# Main function
-def main(titleid, prefix, useCache):
-    global workingproxies, threadsready
-    spamThreads = []
-    if useCache:
-        cache = True
-        print(f"Got {len(cache_proxlist)} proxies from cache.")
-        workingproxies = len(cache_proxlist)
-        proxAmt = len(cache_proxlist)
-    else:
-        cache = False
-        print(f"Got {len(proxlist)} proxies.")
-        workingproxies = len(proxlist)
-        proxAmt = len(proxlist)
-
-    # Create a maximum of 100 threads
-    max_threads = 100
-    print(f"Creating up to {max_threads} threads.")
-    for i in range(min(proxAmt, max_threads)):
+      try:
+        response = requests.post(
+          url=playfabAPIRoute,
+          json=payload,
+          proxies=proxies
+        )
+      except:
         if useCache:
-            randProxy = get_cached_proxy(i)
+          badProxy = spamProxy in cache_proxlist
+          del cache_proxlist[badProxy]
+          cache_proxies(cache_proxlist)
         else:
-            randProxy = get_http_proxy(i)
-        spamThreads.append(threading.Thread(target=spamPlayfab, args=(titleid, prefix, randProxy, cache)))
-    
-    print("Starting threads.")
-    threadsready = True
-    for thread in spamThreads:
-        print(f"Thread Started: {thread}")
-        thread.start()
-    
-    print(f"{Fore.WHITE}[{Fore.LIGHTGREEN_EX}SUCCESS{Fore.WHITE}]{Fore.RESET} All threads ready!")
+          badProxy = spamProxy in proxlist
+          del proxlist[badProxy]
+          cache_proxies(proxlist)
+        workingproxies -= 1
+        break
 
-# Input
-titleid = input(f"{Fore.WHITE}[{Fore.BLUE}CONSOLE{Fore.WHITE}]{Fore.RESET} Title ID to Spam: ")
-prefix = input(f"{Fore.WHITE}[{Fore.BLUE}CONSOLE{Fore.WHITE}]{Fore.RESET} Custom ID Prefix: ")
+      responseJson = response.json()
+        
+      time.sleep(0.35)
 
-# Start the process
-main(titleid, prefix, useCache=False)
+
+      if responseJson['code'] == 200:
+        print(f"{Fore.WHITE}[{Fore.LIGHTGREEN_EX}SUCCESS{Fore.WHITE}]{Fore.RESET} ACCOUNT CREATED : {responseJson['data']['EntityToken']['Entity']['Id']}  WITH CUSTOM ID:  {cid}\n".format(accid=responseJson['data']['EntityToken']['Entity']['Id'], customid=cid, proxy=spamProxy))
+        print(f"{Fore.WHITE}[{Fore.BLUE}CONSOLE{Fore.WHITE}]{Fore.RESET}  Amount Of Accounts Spammed: {accpersec}\n".format(acc_sec=accpersec))
+        accpersec += 1
+      if responseJson['code'] == 429:
+        print(f"{Fore.WHITE}[{Fore.RED}ERROR{Fore.WHITE}]{Fore.RESET}| {spamProxy} Has Been rate Limited Or The Current Playfab Storage IS FULL\n".format(proxy=spamProxy))
+      elif 'error' in responseJson:
+        if responseJson['error'] == 'EvaluationModePlayerCountExceeded':
+          print("Playfab Is Full")
+        else:
+          print("error:\n%s"%str(responseJson))
+
+def main(titleid, prefix, useCache):
+  global workingproxies
+  global threadsready
+  spamThreads = []
+  if useCache:
+    cache = True
+    print("got {proxyamt} proxies".format(proxyamt=len(cache_proxlist)))
+    workingproxies = len(cache_proxlist)
+    proxAmt = len(cache_proxlist)
+  else:
+    cache = False
+    print("got {proxyamt} proxies".format(proxyamt=len(proxlist)))
+    workingproxies = len(proxlist)  
+    proxAmt = len(proxlist)
+  print("MAKING THREADS")
+  for i in range(proxAmt):
+    if useCache:
+      randProxy = get_cached_proxy(i)
+    else:
+      randProxy = get_http_proxy(i)
+    spamThreads.append(threading.Thread(target=spamPlayfab, args=(titleid,prefix,randProxy,cache,)))
+  print("Starting Threads")
+  threadsready = True
+  for thread in spamThreads:
+    print(f"Thread Started: {thread}".format(thread=thread))
+    thread.start()
+  print(f"{Fore.WHITE}[{Fore.LIGHTGREEN_EX}SUCCESS{Fore.WHITE}]{Fore.RESET}  All threads ready!")
+
+
+time.sleep(3)
+
+
+
+
+
+spammerscrim = f"""
+
+{Fore.LIGHTBLACK_EX}
+{Fore.LIGHTWHITE_EX}    ░▒▓███████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒
+{Fore.LIGHTBLACK_EX}  ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+{Fore.LIGHTWHITE_EX}  ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+{Fore.LIGHTBLACK_EX}   ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓██████▓▒░  
+{Fore.LIGHTWHITE_EX}         ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+{Fore.LIGHTBLACK_EX}         ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+{Fore.LIGHTWHITE_EX}  ░▒▓███████▓▒░   ░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+                                                            
+                                                            
+
+                                              
+                                              
+
+                                                                                                          
+
+
+
+"""
+
+os.system('cls')
+print(spammerscrim)
+
+titleid = str(input(f"{Fore.WHITE}[{Fore.BLUE}CONSOLE{Fore.WHITE}]{Fore.RESET} Title ID TO Spam: "))
+pref = input(f"{Fore.WHITE}[{Fore.BLUE}CONSOLE{Fore.WHITE}]{Fore.RESET} Custom ID: ")
+cacheProxy = False
+main(titleid, pref, cacheProxy)
+spamming = True
